d3b7e5f880d7eee33c79f87296312eee674a8e8d
Achmad Rifaldi <achmad.rifaldi@bts.id>
2022-06-22 06:47:14 +0700

    Update datasource path

--- datasource.ts
+++ datasource.ts
@@ -1,12 +1,15 @@
+import * as dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
+
+dotenv.config({ path: __dirname + '/.env' });
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'kode_pos_service',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT),
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
   entities: ['dist/**/*.entity.{ts,js}'],
   migrations: ['dist/db/migrations/*.{ts,js}'],
   subscribers: [],
